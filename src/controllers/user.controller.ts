ff430a71af7203bb435247bf6914f46a828f9293
joelrodriguesvieira <joel2j39@gmail.com>
2024-12-27 17:56:16 -0300

    feat(signup): change cryptography method from crypto to hash

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,24 +6,24 @@ import { ValidationError } from "../middlewares/validation.middleware";
 export class UserController {
     static async getUsers(req: Request, res: Response, next: NextFunction) {
         try {
-            const users = await UserService.getUsers();
-            res.status(200).json({ users });
+            const users = await UserService.getUsers()
+            res.status(200).json({ users })
         } catch (error) {
-            next(error);
+            next(error)
         }
     }
 
     static async createUser(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            const data: CreateUserDTO = req.body;
-            await UserService.createUser(data);
-            res.status(201).json({ message: "User created successfully" });
+            const data: CreateUserDTO = req.body
+            await UserService.createUser(data)
+            res.status(201).json({ message: "User created successfully" })
         } catch (err) {
             if (err instanceof ValidationError) {
-                res.status(400).json({ message: err.message });
-                return;
+                res.status(400).json({ message: err.message })
+                return
             }
-            next(err);
+            next(err)
         }
     }
 }

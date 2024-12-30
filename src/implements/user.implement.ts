5926f57858e89a1a9ab1b336930142f2e117f129
joelrodriguesvieira <joel2j39@gmail.com>
2024-12-30 14:30:53 -0300

    feature: add abstract method in class user implement

--- src/implements/user.implement.ts
+++ src/implements/user.implement.ts
@@ -3,4 +3,6 @@ import { User } from "../entity/User";
 export abstract class UserRepository {
     abstract getUsers(): Promise<User[] | null>
     abstract createUser(data: User): Promise<any>
+    abstract login(encryptedData: string): Promise<User | null>
+    abstract deleteUser(id: string): Promise<User | null>
 }
